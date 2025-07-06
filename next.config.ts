5152a28ae5dec6efe9c708405bfba24d782e8462
uday-bhatiya <udaybhatiya31@gmail.com>
2025-07-06 21:04:31 +0530

    refactor: add ESLint configuration and enable React strict mode

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, 
+  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
+
